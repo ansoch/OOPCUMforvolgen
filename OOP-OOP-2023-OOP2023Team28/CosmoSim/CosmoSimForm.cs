257b6c7f39c3c8ec660bcbf1cddf6fc87d9d4eb2
Aleksandr Sochugov <ansochnn@gmail.com>
2023-11-18 21:20:05 +0700

    exdededeeeeeee

---
+++ OOP-OOP-2023-OOP2023Team28/CosmoSim/CosmoSimForm.cs
@@ -0,0 +1,15 @@
+namespace CosmoSim
+{
+    public partial class CosmoSimForm : Form
+    {
+        public CosmoSimForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
